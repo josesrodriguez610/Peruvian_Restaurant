8d05d793a88637a72cae53afede2c0b1caefdb6e
josesrodriguez610 <josesrodriguez610@gmail.com>
2021-03-22 22:32:30 -0500

    (add) Products section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles.js";
 import Hero from "./components/Hero";
+import Products from "./components/Products/index.js";
+import { productData } from "./components/Products/data.js";
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="Choose your favorite" data={productData} />
     </Router>
   );
 }
